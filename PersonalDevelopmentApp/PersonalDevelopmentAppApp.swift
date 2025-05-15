7fe8ef0fcabdb2a1de3c3c8b2f38c7a4301197e9
Dancilia Kaye-Cee Harmon <dancilia.harmon@techconsulting.tech>
2025-05-15 09:10:44 -0400

    Initial Commit

---
+++ PersonalDevelopmentApp/PersonalDevelopmentAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalDevelopmentAppApp.swift
+//  PersonalDevelopmentApp
+//
+//  Created by Dancilia Harmon   on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalDevelopmentAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
